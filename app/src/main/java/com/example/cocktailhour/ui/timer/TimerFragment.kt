2458f320dc41658a8eede7fd3c2ceecdc53f1718
ltellis <luke.tellis@aroflo.com>
2020-10-03 09:49:49 +1000

    Added basic timer functionality

--- app/src/main/java/com/example/cocktailhour/ui/timer/TimerFragment.kt
+++ app/src/main/java/com/example/cocktailhour/ui/timer/TimerFragment.kt
@@ -1,9 +1,12 @@
 package com.example.cocktailhour.ui.timer
 
-import android.os.Bundle
+import android.content.Context
+import android.os.*
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Chronometer
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
@@ -26,6 +29,32 @@ class TimerFragment : Fragment() {
         timerViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
+
+        val timerBtn = root.findViewById<Button>(R.id.timerBtn)
+        val view_timer = root.findViewById<Chronometer>(R.id.view_timer)
+        view_timer.text = "00:15"
+
+        timerBtn.setOnClickListener {
+            view_timer.isCountDown = true
+            view_timer.base = SystemClock.elapsedRealtime() + 15000
+            view_timer.start()
+        }
+
+        view_timer.setOnChronometerTickListener {
+            if (it.text.toString().equals("00:00")) {
+
+                Handler(Looper.getMainLooper()).postDelayed({
+                    // Reset Chronometer
+                    it.onChronometerTickListener = null
+                    it.stop()
+
+                    view_timer.base = SystemClock.elapsedRealtime() + 15000
+                    val vibrator = context?.getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
+                    vibrator.vibrate(VibrationEffect.createOneShot(200, VibrationEffect.DEFAULT_AMPLITUDE))
+                }, 1000)
+            }
+        }
+
         return root
     }
 }
